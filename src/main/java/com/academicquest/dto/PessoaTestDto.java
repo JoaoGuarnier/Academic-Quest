237bc23a88e5ce5fa1a25ce903d0dede0b8f2341
JoaoGuarnier <joaocarlosguarnier@hotmail.com>
2022-04-13 14:15:22 -0300

    adicionando metodo post para teste

---
+++ src/main/java/com/academicquest/dto/PessoaTestDto.java
@@ -0,0 +1,12 @@
+package com.academicquest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PessoaTestDto {
+	private String nome;
+}
