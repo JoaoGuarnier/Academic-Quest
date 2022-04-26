cb1faf6ab693cbe69b5c1a456f92a8d23c8d561a
JoaoGuarnier <joaocarlosguarnier@hotmail.com>
2022-04-26 20:45:31 -0300

    removendo link h2 spring security

--- src/main/java/com/academicquest/config/ResourceServerConfig.java
+++ src/main/java/com/academicquest/config/ResourceServerConfig.java
@@ -23,7 +23,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     private JwtTokenStore tokenStore;
   
 
-    private static final String[] PUBLIC = { "/oauth/token", "/teste/**" , "/h2-console/**" };
+    private static final String[] PUBLIC = { "/oauth/token", "/teste/**" };
 
     private static final String[] OPERATOR_OR_ADMIN = { };
 
