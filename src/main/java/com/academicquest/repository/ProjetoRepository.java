88c65342d7347704b4cd8fe399240c3f1a74d50c
JoaoGuarnier <joaocarlosguarnier@hotmail.com>
2022-06-10 16:39:32 -0300

    modificando query method projeto

--- src/main/java/com/academicquest/repository/ProjetoRepository.java
+++ src/main/java/com/academicquest/repository/ProjetoRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 
 public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
 
-    @Query(value = "select * from tb_projeto where materia_id = :materiaId", nativeQuery = true)
-    List<Projeto> findyByMateriaId(Long materiaId);
-
+    List<Projeto> findByMateriaId(Long materiaId);
 }
