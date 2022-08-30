cabdece4df06adce9c63764952e3720525cc088f
joaoguarnier <joaocarlosguarnier@hotmail.com>
2022-08-30 18:59:22 -0300

    test db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,98 @@
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (1,'Joao', 'Guarnier', 'joao@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (2,'Leon', 'Miranda', 'leon@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (3,'Gabriel', 'Silva', 'gabriel@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (4,'Nicolas', 'Lee', 'nicolee@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (5,'Paola', 'Hoffmann', 'paola@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (6,'Marcos', 'Fagundes', 'marfun@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (7,'Nathalia', 'Lopes', 'na_lopes@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (8,'Ricardo', 'silva', 'ricardo_xbox@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (9,'Clara', 'Silva', 'clarinha@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (10,'Juan', 'Sampaio', 'juanzera@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (11,'Tancredo', 'Neves', 'tancredex257@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 1);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (12,'Nestor', 'Cerveroc', 'eyedown@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (13,'Liz', 'Guarnier', 'liz_guarnier@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (14,'Neymar', 'Junior', 'neyzika@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (15,'Lebrao', 'Jaime', 'celta2010@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (16,'Cuca', 'Beludo', 'beludinho@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (17,'Deide', 'Costa', 'rio_fevereiro@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (18,'Paco', 'Leiva', 'pakex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (19,'Limber', 'Leitor', 'jhonatan@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno, turma_id) VALUES (20,'Paulo', 'Jorge', 'paul_jorge@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, 2);
+
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno) VALUES (21,'Fabinho', 'Silva', 'fabinho@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 0);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno) VALUES (22,'Victor', 'Inacio', 'vitinho@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 0);
+INSERT INTO tb_user (id, first_name, last_name, email, password, is_aluno) VALUES (23,'Cristiano', 'Ronaldo', 'cr7@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 0);
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_ALUNO');
+INSERT INTO tb_role (authority) VALUES ('ROLE_PROFESSOR');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (4, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (5, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (6, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (7, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (8, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (9, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (10, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (11, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (12, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (13, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (14, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (15, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (16, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (17, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (18, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (19, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (20, 1);
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (21, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (22, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (23, 2);
+
+INSERT INTO tb_turma (CURSO, SEMESTRE, COMPLEMENTO) VALUES ('ENGENHARIA DE COMPUTADOR', 3, 'A');
+INSERT INTO tb_turma (CURSO, SEMESTRE, COMPLEMENTO) VALUES ('CIENCIA DE COMPUTADOR', 1, 'B');
+INSERT INTO tb_turma (CURSO, SEMESTRE, COMPLEMENTO) VALUES ('BANCO DE DADOS', 4, 'A');
+INSERT INTO tb_turma (CURSO, SEMESTRE, COMPLEMENTO) VALUES ('ANALISE E DESENVOLVIMENTO DE COMPUTADOR', 2, 'B');
+
+INSERT INTO tb_materia (NOME, PROFESSOR_ID, TURMA_ID) VALUES ('ESTRUTURA DE DADOS', 22,1);
+INSERT INTO tb_materia (NOME, PROFESSOR_ID, TURMA_ID) VALUES ('LINGUAGEM DE COMPUTADOR 1', 23,1);
+INSERT INTO tb_materia (NOME, PROFESSOR_ID, TURMA_ID) VALUES ('DEVOPS', 21,2);
+INSERT INTO tb_materia (NOME, PROFESSOR_ID, TURMA_ID) VALUES ('LINGUAGEM DE COMPUTADOR 2', 23,2);
+
+
+
+INSERT INTO TB_GRUPO (USER_LIDER_ID, NOME, MATERIA_ID) VALUES (1, 'MALUCATIONS', 1);
+
+INSERT INTO TB_GRUPO (USER_LIDER_ID, NOME, MATERIA_ID) VALUES (4, 'DOIDEX', 1);
+INSERT INTO TB_GRUPO (USER_LIDER_ID, NOME, MATERIA_ID) VALUES (7, 'PS3 RODELA', 1);
+
+INSERT INTO TB_GRUPO (USER_LIDER_ID, NOME, MATERIA_ID) VALUES (1, 'MALUCATIONS', 2);
+INSERT INTO TB_GRUPO (USER_LIDER_ID, NOME, MATERIA_ID) VALUES (4, 'DOIDEX', 2);
+INSERT INTO TB_GRUPO (USER_LIDER_ID, NOME, MATERIA_ID) VALUES (7, 'PS3 RODELA', 2);
+
+
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (1,1);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (1,2);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (1,3);
+
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (2,4);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (2,5);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (2,6);
+
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (3,7);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (3,8);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (3,9);
+
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (4,1);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (4,2);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (4,3);
+
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (5,4);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (5,5);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (5,6);
+
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (6,7);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (6,8);
+INSERT INTO TB_GRUPO_USER (GRUPO_ID, USER_ID) VALUES (6,9);
